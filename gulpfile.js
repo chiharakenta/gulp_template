e9992c59e1facdc11c88929b0b2c5b9e7cd9c322
chiharakenta <chiharakenta0519@gmail.com>
2019-03-21 13:28:36 +0900

    'complete'

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,9 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var postcss      = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
-
 var cleanCSS = require('gulp-clean-css');
 var rename   = require("gulp-rename");
 var uglify = require('gulp-uglify');
-var imagemin = require("gulp-imagemin");
-var imageminPngquant = require("imagemin-pngquant");
-var imageminMozjpeg = require("imagemin-mozjpeg");
 
 gulp.task('sass', function() {
   return gulp.src('./sass/**/*.scss')
@@ -18,7 +14,7 @@ gulp.task('sass', function() {
 });
 
 gulp.task('mincss', function() {
-  return gulp.src("./css/*.css")
+  return gulp.src("./css/style.css")
     .pipe(cleanCSS())
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('css/'));
@@ -30,27 +26,7 @@ gulp.task('minjs', function() {
     .pipe(gulp.dest('js/'));
 });
 
-/* var imageminOption = [
-  imageminPngquant({ quality: '65-80' }),
-  imageminMozjpeg({ quality: 85 }),
-  imagemin.gifsicle({
-    interlaced: false,
-    optimizationLevel: 1,
-    colors: 256
-  }),
-  imagemin.jpegtran(),
-  imagemin.optipng(),
-  imagemin.svgo()
-];
-
-gulp.task( 'imagemin', function() {
-  return gulp
-    .src( './img/base/*.{png,jpg,gif,svg}' )
-    .pipe( imagemin( imageminOption ) )
-    .pipe( gulp.dest( './img' ) );
-}); */
-
-gulp.task( 'watch', function() {
-  gulp.watch( './sass/**/*.scss', gulp.series(gulp.task( 'sass' ), gulp.task( 'mincss' ) ) );
+gulp.task( 'default', function() {
+  gulp.watch( './sass/**/*.scss', gulp.series(gulp.task( 'sass' ), gulp.task('mincss')) );
   gulp.watch( './js/**/*.js', gulp.task( 'minjs' ) );
 });
